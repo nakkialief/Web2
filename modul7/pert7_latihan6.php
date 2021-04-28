48835306404eb6d2d6c6d5b3d798f2db524b01dd
nakkialief <nakkialief23@gmail.com>
2021-04-28 22:06:13 +0700

    nakkiweb

---
+++ modul7/pert7_latihan6.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Penggunaan List 2</title>
+</head>
+<body>
+<?php
+$program = array ('Bobo','Doraemon','Spiderman');
+list($majalah,$film) = $program;
+echo " jenis buku & hiburan: ";
+echo "<br>";
+echo "cerpen : $majalah";
+echo "<br>";
+echo "bioskop : $film";
+
+?>
+
+</body>
+</html>
